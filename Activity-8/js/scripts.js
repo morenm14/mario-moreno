2736e0356dfc04ddd49d1ce521c90c7cb0a2c1ee
Mario Moreno <morenm14@gmatc.matc.edu>
2020-11-17 12:15:11 -0600

    added validation

--- Activity-8/js/scripts.js
+++ Activity-8/js/scripts.js
@@ -18,6 +18,8 @@ function closeModal()
     var text = get ("edit-content-text");
     var modal = get("modal-dialog");
     var backdrop = get ("modal-backdrop");
+    var titleMessage = get("edit-title-message");
+    var contentMessage = get ("edit-content-message");
 
     //clear text
     title.value = "";
@@ -26,6 +28,9 @@ function closeModal()
     //hide modal
     modal.classList.remove("visible");
     backdrop.classList.remove("visible");
+    titleMessage.classList.remove("visible");
+    contentMessage.classList.remove("visible");
+    
 
 }
 
@@ -34,12 +39,34 @@ function saveContent()
     var title = get ("edit-title-text");
     var text = get ("edit-content-text");
     var content = get("display-content");
+    var titleMessage = get("edit-title-message");
+    var contentText = get("edit-content-message");
+
+    if(!title.value){
+        titleMessage.textContent ="Please supply a title";
+        titleMessage.classList.add("visible");
+        return;
+    }else {
+        titleMessage.classList.remove("visible");
+    }
+
+    if(!text.value)
+    {
+        
+        contentText.textContent = "Enter a message";
+        contentText.classList.add("visible");
+        return;
+    }else{
+        contentText.classList.remove("visible");
+    }
 
     //create content elements
     var newTitle = document.createElement("h2");
     var newTitleText = document.createTextNode(title.value);
     var newContent = document.createElement("p");
     var newContentText = document.createTextNode(text.value);
+
+
 
     //add elements
 
